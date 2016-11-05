0a7fc50d0454dfdac8bccac970c06fdbb3e0ca13
tipouic <tipouic@yahoo.fr>
2016-11-05 19:33:16 +0100

    Correction bug

--- examples/RxSerialPrint/RxSerialPrint.ino
+++ examples/RxSerialPrint/RxSerialPrint.ino
@@ -1,3 +1,4 @@
+#include <SPI.h>
 #include <NRF24_RX.h>
 #include <nrf24_rx_h8_3d.h>
 #include <nrf24_rx_cx10.h>
@@ -11,62 +12,51 @@ static const int CSN_PIN = 10;
 //#define USE_H8_3D
 
 #if defined(USE_CX10)
-CX10_RX nrf24(CE_PIN, CSN_PIN);
-NRF24_RX *rx = &nrf24;
-static const int protocol = NRF24_RX::CX10A;
-static const int rcChannelCount = CX10_RX::RC_CHANNEL_COUNT;
+	CX10_RX nrf24(CE_PIN, CSN_PIN);
+	NRF24_RX *rx = &nrf24;
+	static const int protocol = NRF24_RX::CX10A;
+	static const int rcChannelCount = CX10_RX::RC_CHANNEL_COUNT;
 #elif defined(USE_H8_3D)
-H8_3D_RX nrf24(CE_PIN, CSN_PIN);
-NRF24_RX *rx = &nrf24;
-static const int protocol = NRF24_RX::H8_3D;
-static const int rcChannelCount = H8_3D_RX::RC_CHANNEL_COUNT;
+	H8_3D_RX nrf24(CE_PIN, CSN_PIN);
+	NRF24_RX *rx = &nrf24;
+	static const int protocol = NRF24_RX::H8_3D;
+	static const int rcChannelCount = H8_3D_RX::RC_CHANNEL_COUNT;
 #endif
 
 uint16_t nrf24RcData[rcChannelCount];
 
 
-void setup()
-{
-  rx->begin(protocol);
+void setup() {
+	rx->begin(protocol);
 
-  Serial.begin(57600);
-  Serial.println("");
-  Serial.print("Receiver Starting, protocol: ");
-  Serial.println(String(rx->getProtocolString()));
-  Serial.println("");
-  delay(100);
+	Serial.begin(57600);
+	Serial.println("");
+	Serial.print("Receiver Starting, protocol: ");
+	Serial.println(String(rx->getProtocolString()));
+	Serial.println("");
+	delay(100);
 }
 
-void printRcData()
-{
-  Serial.println("");
-  Serial.print("rol=");
-  Serial.print(nrf24RcData[NRF24_ROLL]);
-  Serial.print(",");
-  Serial.print("pit=");
-  Serial.print(nrf24RcData[NRF24_PITCH]);
-  Serial.print(",");
-  Serial.print("thr=");
-  Serial.print(nrf24RcData[NRF24_THROTTLE]);
-  Serial.print(",");
-  Serial.print("yaw=");
-  Serial.print(nrf24RcData[NRF24_YAW]);
+void printRcData() {
+	Serial.println("");
+	Serial.print("rol=");	Serial.print(nrf24RcData[NRF24_ROLL]);		Serial.print(",");
+	Serial.print("pit=");	Serial.print(nrf24RcData[NRF24_PITCH]);		Serial.print(",");
+	Serial.print("thr=");	Serial.print(nrf24RcData[NRF24_THROTTLE]);	Serial.print(",");
+	Serial.print("yaw=");	Serial.print(nrf24RcData[NRF24_YAW]);
 }
 
-void loop()
-{
-  static bool printedBinding = false;
-  const NRF24_RX::received_e dataReceived = rx->dataReceived();
-  if (dataReceived == NRF24_RX::RECEIVED_BIND) {
-    if (printedBinding == false) {
-      Serial.print("Binding");
-      printedBinding = true;
-    }
-    Serial.print(".");
-  } else if (dataReceived == NRF24_RX::RECEIVED_DATA) {
-    rx->setRcDataFromPayload(nrf24RcData);
-    printRcData();
-  }
-  delay(1);
+void loop() {
+	static bool printedBinding = false;
+	const NRF24_RX::received_e dataReceived = rx->dataReceived();
+	if (dataReceived == NRF24_RX::RECEIVED_BIND) {
+		if (printedBinding == false) {
+			Serial.print("Binding");
+			printedBinding = true;
+		}
+		Serial.print(".");
+	} else if (dataReceived == NRF24_RX::RECEIVED_DATA) {
+		rx->setRcDataFromPayload(nrf24RcData);
+		printRcData();
+	}
+	delay(1);
 }
-
--- examples/RxServos/RxServos.ino
+++ examples/RxServos/RxServos.ino
@@ -1,6 +1,7 @@
+#include <SPI.h>
 #include <NRF24_RX.h>
-#include <nrf24_cx10.h>
-#include <nrf24_h8_3d.h>
+#include <nrf24_rx_cx10.h>
+#include <nrf24_rx_h8_3d.h>
 #include <Servo.h>
 
 // NRF24L01 pins
@@ -12,15 +13,15 @@ static const int CSN_PIN = 10;
 #define USE_H8_3D
 
 #if defined(USE_CX10)
-CX10 nrf24(CE_PIN, CSN_PIN);
-NRF24_RX *rx = &nrf24;
-static const int protocol = NRF24_RX::CX10;
-static const int rcChannelCount = CX10::RC_CHANNEL_COUNT;
+	CX10_RX nrf24(CE_PIN, CSN_PIN);
+	NRF24_RX *rx = &nrf24;
+	static const int protocol = NRF24_RX::CX10A;
+	static const int rcChannelCount = CX10_RX::RC_CHANNEL_COUNT;
 #elif defined(USE_H8_3D)
-H8_3D nrf24(CE_PIN, CSN_PIN);
-NRF24_RX *rx = &nrf24;
-static const int protocol = NRF24_RX::H8_3D_H20;
-static const int rcChannelCount = H8_3D::RC_CHANNEL_COUNT;
+	H8_3D_RX nrf24(CE_PIN, CSN_PIN);
+	NRF24_RX *rx = &nrf24;
+	static const int protocol = NRF24_RX::H8_3D;
+	static const int rcChannelCount = H8_3D_RX::RC_CHANNEL_COUNT;
 #endif
 
 uint16_t nrf24RcData[rcChannelCount];
@@ -35,37 +36,37 @@ Servo servo2;
 
 void setup() 
 {
-  rx->begin(protocol);
+	rx->begin(protocol);
 
-  servo1.attach(SERVO1_PIN);
-  servo2.attach(SERVO2_PIN);
+	servo1.attach(SERVO1_PIN);
+	servo2.attach(SERVO2_PIN);
 
-  Serial.begin(57600);
-  Serial.print("Receiver Starting, protocol: ");
-  Serial.println(rx->getProtocol());
-  Serial.println("");
-  delay(100);
+	Serial.begin(57600);
+	Serial.print("Receiver Starting, protocol: ");
+	Serial.println(rx->getProtocol());
+	Serial.println("");
+	delay(100);
 }
 
 void loop() 
 {
-  const NRF24_RX::received_e dataReceived = rx->dataReceived();
-  if (dataReceived == NRF24_RX::RECEIVED_DATA) {
-    // set the radio control data into the nrf24RcData[] array
-    rx->setRcDataFromPayload(nrf24RcData);
+	const NRF24_RX::received_e dataReceived = rx->dataReceived();
+	if (dataReceived == NRF24_RX::RECEIVED_DATA) {
+		// set the radio control data into the nrf24RcData[] array
+		rx->setRcDataFromPayload(nrf24RcData);
 
-    // set servo1 from the throttle value
-    int throttle = nrf24RcData[NRF24_THROTTLE];
-    // scale throttle to value between 0 and 180 for servo
-    int s1 = map(throttle, PWM_RANGE_MIN, PWM_RANGE_MAX, 0, 180);
-    servo1.write(s1);
+		// set servo1 from the throttle value
+		int throttle = nrf24RcData[NRF24_THROTTLE];
+		// scale throttle to value between 0 and 180 for servo
+		int s1 = map(throttle, PWM_RANGE_MIN, PWM_RANGE_MAX, 0, 180);
+		servo1.write(s1);
 
-    // set servo2 from the yaw value
-    int yaw = nrf24RcData[NRF24_YAW];
-    // scale yaw to value between 0 and 180 for servo
-    int s2 = map(yaw, PWM_RANGE_MIN, PWM_RANGE_MAX, 0, 180);
-    servo2.write(s2);
-  }
-  delay(1); // delay for 1 millisecond before next request to receiver
+		// set servo2 from the yaw value
+		int yaw = nrf24RcData[NRF24_YAW];
+		// scale yaw to value between 0 and 180 for servo
+		int s2 = map(yaw, PWM_RANGE_MIN, PWM_RANGE_MAX, 0, 180);
+		servo2.write(s2);
+	}
+	delay(1); // delay for 1 millisecond before next request to receiver
 }
 
